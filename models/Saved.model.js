324fb1455e4528f200c901d5ad9ec02e345291b9
Rakhim <abdurrohim9505@gmail.com>
2021-07-30 00:55:45 +0300

    Saved

---
+++ models/Saved.model.js
@@ -0,0 +1,12 @@
+const { mongoose } = require("mongoose");
+
+const savedSchema = mongoose.Schema({
+  postId: {
+    type: mongoose.Schema.Types.objectId,
+    ref: "Post",
+  },
+});
+
+const Saved = mongoose.model("Saved", savedSchema);
+
+module.exports = Saved;
